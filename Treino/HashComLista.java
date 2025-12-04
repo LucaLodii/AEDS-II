2f6354606dd35986ca2196b06474086de5973efb
LucaLodii <luca.lodi.ll53@gmail.com>
2025-12-03 21:15:17 -0300

    fix: implementa solução correta para o problema

--- Treino/HashComLista.java
+++ Treino/HashComLista.java
@@ -1,23 +1,90 @@
 package Treino;
 import java.util.Scanner;
 
-public class HashComLista {
+class Carro {
+    String placa, modelo, tipo, chassi;
 
-    private static int hash(String placa, int N){
-        int posicao = 0;
-
-        for(int i = 0; i < placa.length(); i++) {
-            posicao += palaca.charAt(i);
-        }
-        posicao = posicao % N;
-        return posicao;
+    void ler(String linha) {
+        String[] partes = linha.split(",");
+        placa = partes[0];
+        modelo = partes[1];
+        tipo = partes[2];
+        chassi = partes[3];
     }
+
+    void imprimir() {
+        System.out.println(placa + " " + modelo + " " + tipo + " " + chassi);
+    }
+}
+
+class No {
+    Carro carro;
+    No proximo;
+
+    public No(Carro c) {
+        this.carro = c;
+        this.proximo = null;
+    }
+}
+
+public class HashComLista {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
-        int N = scan.nextInt();
-        String placa = scan.nextLine();
-
-        hash(placa, N);
+        Scanner sc = new Scanner(System.in);
+        
+        if (sc.hasNextLine()) {
+            int N = Integer.parseInt(sc.nextLine());
+            No[] tabela = new No[N];
+            
+            String linha = sc.nextLine();
+            while (!linha.equals("FIM")) {
+                Carro c = new Carro();
+                c.ler(linha);
+                
+                int soma = 0;
+                for (int i = 0; i < c.placa.length(); i++) {
+                    soma += c.placa.charAt(i);
+                }
+                int pos = soma % N;
+                
+                No novo = new No(c);
+                novo.proximo = tabela[pos];
+                tabela[pos] = novo;
+                
+                linha = sc.nextLine();
+            }
+            
+            if (sc.hasNextLine()) {
+                linha = sc.nextLine();
+                while (!linha.equals("FIM_CONSULTA")) {
+                    int soma = 0;
+                    for (int i = 0; i < linha.length(); i++) {
+                        soma += linha.charAt(i);
+                    }
+                    int pos = soma % N;
+                    
+                    No atual = tabela[pos];
+                    boolean encontrado = false;
+                    
+                    while (atual != null && !encontrado) {
+                        if (atual.carro.placa.equals(linha)) {
+                            atual.carro.imprimir();
+                            encontrado = true;
+                        }
+                        atual = atual.proximo;
+                    }
+                    
+                    if (!encontrado) {
+                        System.out.println("VEICULO NAO CADASTRADO");
+                    }
+                    
+                    if (sc.hasNextLine()) {
+                        linha = sc.nextLine();
+                    } else {
+                        linha = "FIM_CONSULTA";
+                    }
+                }
+            }
+        }
+        sc.close();
     }
 }
